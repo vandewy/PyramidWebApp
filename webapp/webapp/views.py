502d6c58004708a8383c95589e71caa449d91f62
Ryan V <vandewynkel@gmail.com>
2018-07-28 18:32:58 -0500

    Work on CSS Cache Busting

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
-
+import webapp.utils
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'WebApp'}
+
+def extend_model(model_dict):
+    model_dict['build_cache_id'] = webapp.utils.build_cache_id
+    return
